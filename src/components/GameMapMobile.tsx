89eb1b0a250d3bf6898e62c8c4ba3aa8efd35766
Dominic Lemoine de Martigny <dodolem0306@gmail.com>
2025-08-05 00:55:50 -0400

    map mobile fix

--- src/components/GameMapMobile.tsx
+++ src/components/GameMapMobile.tsx
@@ -215,7 +215,10 @@ return (
       {/* Submit Button - Bottom Right (only when guess is placed) */}
       {userGuess && (
         <button
-          onClick={onSubmitGuess}
+          onClick={() => {
+            onSubmitGuess?.();
+            onToggleExpanded();
+          }}
           className="absolute bottom-4 right-4 z-50 bg-yellow-400 hover:bg-yellow-500 text-black px-4 py-2.5 rounded-xl font-bold text-xl border-4 border-black shadow-lg transition-all duration-200"
         >
           Submit Guess!
