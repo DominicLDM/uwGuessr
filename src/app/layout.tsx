62e8a29184f4d34fd6a7e2db262d268b8851f2c0
Dominic Lemoine de Martigny <dodolem0306@gmail.com>
2025-08-06 22:07:09 -0400

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,45 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="apple-mobile-web-app-title" content="uwGuessr" />
+        {/* Open Graph */}
+        <meta property="og:title" content="uwGuessr" />
+        <meta property="og:description" content="A UWaterloo guessing game!" />
+        <meta property="og:image" content="https://uwguessr.com/preview.png" />
+        <meta property="og:url" content="https://uwguessr.com" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="uwGuessr" />
+        <meta name="twitter:description" content="A UWaterloo guessing game!" />
+        <meta name="twitter:image" content="https://uwguessr.com/preview.png" />
+
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
+
+        {/* Favicon (ICO, PNG, SVG) */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon1.png" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/svg+xml" href="/icon0.svg" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/icon1.png" />
+        <link rel="icon" type="image/png" sizes="64x64" href="/icon1.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="128x128" href="/icon1.png" />
+        <link rel="icon" type="image/png" sizes="256x256" href="/icon1.png" />
+
+        {/* Microsoft Tile */}
+        <meta name="msapplication-TileColor" content="#ffc40d" />
+        <meta name="msapplication-TileImage" content="/icon1.png" />
+
+        {/* Manifest for PWA */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Theme color for mobile browsers */}
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
